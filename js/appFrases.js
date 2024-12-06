987ac60ac46fd56c5144b3e849849fff879a0a88
Alejandro Jimenez <alejandro.jmzl33@gmail.com>
2024-12-06 00:42:54 +0000

    Taller15

--- js/appFrases.js
+++ js/appFrases.js
@@ -1,24 +1,66 @@
 
 const frases = [
-    {frase:'El que madruga Dios le ayuda',Autor:'Pepito Perez'},
-    {frase:'Si el rio suena es porque piedras trae',Autor:'Juan Palomino'},
-    {frase:'El que mucho abarca poco aprieta',Autor:'Jhon Tevez'},
-    {frase:'Mas vale pajaro en mano que ciento volando',Autor:'Alexis Canada'},
-    {frase:'Perro que ladra no muerte',Autor:'Juan Tevez'},
-    {frase:'Los saposs mueren aplastados',Autor:'Alexis Texas'}
+    { frase: 'El que madruga Dios le ayuda', autor: 'Pepito Perez' },
+    { frase: 'Si el rio suena es porque piedras trae', autor: 'Juan Palomino' },
+    { frase: 'El que mucho abarca poco aprieta', autor: 'Jhon Tevez' },
+    { frase: 'Mas vale pajaro en mano que ciento volando', autor: 'Alexis Canada' },
+    { frase: 'Perro que ladra no muerte', autor: 'Juan Tevez' },
+    { frase: 'Los saposs mueren aplastados', autor: 'Alexis Texas' }
 ]
 
 
-
-const app = Vue.createApp({/* backtick ALT + 96*/
-
+const app = Vue.createApp({
 
     methods: {
-        
+        agregarFrase() {
+            console.log(this.frase);
+            console.log(this.autor);
+
+            const nuevaFrase = {
+                frase: this.frase,
+                autor: this.autor
+            }
+
+            this.listaFrases.unshift(nuevaFrase);
+        },
+
+        agregarFraseFinal() {
+            console.log(this.frase);
+            console.log(this.autor);
+
+            const nuevaFrase = {
+                frase: this.frase,
+                autor: this.autor
+            }
+
+            this.listaFrases.push(nuevaFrase);
+        },
+        eventoKeyPress({charCode,cancelable,keyCode,key,target}) {
+            if (key === 'Enter') {
+                console.log('Evento');
+                console.log(charCode);
+                console.log(cancelable);
+                console.log(keyCode);
+                console.log(key);
+
+                console.log(target.baseURI);
+                this.agregarFraseFinal();
+
+
+            }
+        },
+        eventoKeyPressModificador() {
+         
+            this.agregarFraseFinal();
+    }
+
     },
     data() {
         return {
-            listafrases:frases
+            listaFrases: frases,
+            frase: null,
+            //autor: 'sin autor'
+            autor: null
         }
     }
 })
